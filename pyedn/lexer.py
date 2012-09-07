dca6c0eff0bc7a9ae71a6b3d78422ea8774f968c
J Kenneth King <james@agentultra.com>
2012-09-07 10:51:20 -0400

    Added second test case to parse two nil

--- pyedn/lexer.py
+++ pyedn/lexer.py
@@ -7,5 +7,29 @@ from collections import namedtuple
 Token = namedtuple("Token", "text tag position")
 
 
+NIL = "NIL"
+
+
+TOKEN_EXPRS = ((r"[ \n\t]+", None),
+               (r"nil", NIL),)
+
+
 def lex(bytes):
-    return [Token("nil", "NIL", 0)]
+    pos = 0
+    tokens = []
+    while pos < len(bytes):
+        match = None
+        for pattern, tag in TOKEN_EXPRS:
+            regex = re.compile(pattern)
+            match = regex.match(bytes, pos)
+            if match:
+                text = match.group(0)
+                if tag:
+                    tokens.append(Token(text, tag, pos))
+                break
+        if not match:
+            raise ValueError("Illegal character {0} in position: {1}\n".format(
+                bytes[pos], pos))
+        else:
+            pos = match.end(0)
+    return tokens
